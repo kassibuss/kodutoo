51ed7bf7f2a78ef24a4bc591db35e01dc67fdab6
kassibuss <81193965+kassibuss@users.noreply.github.com>
2022-10-15 13:45:20 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,9 +1,12 @@
 function checkTemp(x) {
 if (x >= 0 && x <= 20) {
-    return console.log("-1") //Liiga külm
+    console.log("-1") //Liiga külm
+    return -1
   } else if (x >= 21 && x <= 40) {
-    return console.log("0") //Paras
+    console.log("0") //Paras
+    return 0 
   } else if (x >= 41 && x <= 60){
-    return console.log("1") //Liiga kuum
+    console.log("1") //Liiga kuum
+    return 1
   }
 }
